6eb6d26296a8abfdf25486ab1555c2d7074a49c4
whatnow1234 <huyquangtran12@gmail.com>
2023-11-12 16:07:26 +0700

    Change name

---
+++ Controllers/LabController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualLab.Controllers
+{
+    public class LabController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
